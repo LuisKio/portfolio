0ddb486b0b1d8fd2a4905abbe848752f3628abbd
luis eduardo <luislacln@gmail.com>
2022-11-07 12:46:03 -0600

    Actualizacion del main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,19 @@ const toggleMenuElement = document.getElementById('toggle-menu');
 const mainMenuElement = document.getElementById('main-menu');
 const navigationElements = document.querySelectorAll("#main-menu li");
 
-toggleMenuElement.addEventListener('click', () => {
+toggleMenuElement.addEventListener('click', (e) => {
     toggleMenuElement.classList.toggle('active-menu');
     mainMenuElement.classList.toggle('main-menu-show');
 })
 
-navigationElements.forEach(element => {
-    element.addEventListener('click', () => {
-        toggleMenuElement.classList.toggle('active-menu');
-        mainMenuElement.classList.toggle('main-menu-show');
-    })
+mainMenuElement.addEventListener('click', (e) => {
+    toggleMenuElement.classList.toggle('active-menu');
+    mainMenuElement.classList.toggle('main-menu-show');
 });
+
+
+
+
 
 //SE LE ASIGNA UNA CLASE AL HEADER QUE CONTIENE EL NAV CUANDO EL USUARIO ESTA EN UNA POSICION EN EL EJE Y MAYOR A 0
 window.addEventListener('scroll', () => {
